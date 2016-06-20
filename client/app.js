70ab0d3c125e2ed5eacf06381451c29de0cb3411
Mike Simmonds <mike@simmo.me>
2016-06-20 06:58:32 +0100

    Web app support

--- client/app.js
+++ client/app.js
@@ -4,6 +4,10 @@ import App from 'components/app'
 import { Provider } from 'react-redux'
 import store from 'utilities/store'
 
+if (window.navigator.standalone) {
+    document.documentElement.classList.add('standalone')
+}
+
 ReactDom.render(
     <Provider store={store}>
         <App />
--- index.js
+++ index.js
@@ -108,7 +108,11 @@ app.all('/', (req, res) => {
             <head>
                 <title>Time</title>
                 <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,600,700" rel="stylesheet">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+                <meta name="mobile-web-app-capable" content="yes">
+                <meta name="apple-mobile-web-app-capable" content="yes">
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+                <meta name="apple-mobile-web-app-title" content="Reflect">
                 <link href="/app.css" rel="stylesheet">
             </head>
             <body>
