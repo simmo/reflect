37f3319057afbed1783b6d4f2b96766fe2f763e5
Mike Simmonds <mike@simmo.me>
2016-06-27 18:42:54 +0100

    Removing greeting

--- client/reducers/time.js
+++ client/reducers/time.js
@@ -7,29 +7,17 @@ const momentBetweenToSecond = (time, from, to) => time.isSameOrAfter(from, 'seco
 
 const formatTime = (time, location = ['50.9977', '-0.1037']) => {
     let now = moment(time)
-    let greeting
 
-    const splitAfternoon = 12 // 24hr time to split the afternoon
-    const splitEvening = 17 // 24hr time to split the evening
     const hours = parseInt(now.format('H'), 10)
     const getSunTimes = SunCalc.getTimes(time, location[0], location[1])
 
+    // Calculate sun times
     const sunTimeMoments = Object.keys(getSunTimes).reduce((obj, name) => {
         obj[name] = moment(getSunTimes[name])
         return obj
     }, {})
 
-    // Calculate greeting
-    if (hours >= splitAfternoon && hours <= splitEvening) {
-        greeting = 'afternoon'
-    } else if (hours >= splitEvening) {
-        greeting = 'evening'
-    } else {
-        greeting = 'morning'
-    }
-
     return {
-        greeting,
         timestamp: time,
         hours,
         minutes: now.format('mm'),
